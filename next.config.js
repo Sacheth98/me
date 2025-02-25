8b11ce055d2a4bf88012d49b2ec669e21ef6d908
Sacheth98 <46384357+Sacheth98@users.noreply.github.com>
2025-02-25 01:35:37 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
   images: {
     unoptimized: true, // Required for static export
   },
+  basePath: "/me", // 🔹 Must match your repository name exactly
+  assetPrefix: "/me/", // 🔹 Ensures assets load correctly
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glb|gltf)$/,
